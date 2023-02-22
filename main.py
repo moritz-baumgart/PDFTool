7a8ecccfe120f20f5313afa040fa1703d772a37b
orangeUNNIX <baumgartmoritz@web.de>
2023-02-22 01:44:55 +0100

    Added operation selection

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+from PyInquirer import prompt
+from merge import merge
+from reorderDualPage import reorderDualPage
+from deletePages import deletePages
+
+def main():
+
+    choices = {
+        '- Merge files': merge,
+        '- Reorder dual page scan': reorderDualPage,
+        '- Delete single pages': deletePages
+    }
+
+    question = [
+        {
+            'type': 'list',
+            'name': 'operation',
+            'message': 'What do you want to do?',
+            'choices': choices.keys()
+        }
+    ]
+
+    operation = prompt(question)['operation']
+    choices[operation]()
 
 
-print("Hello world!")
+if __name__ == '__main__':
+    main()
+
+
---
+++ reorderDualPage.py
@@ -0,0 +1,4 @@
+
+
+def reorderDualPage():
+    raise NotImplementedError()
