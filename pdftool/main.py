546b5dc1c9ef99e526946a5c5b011eaa8ea2fe95
orangeUNNIX <baumgartmoritz@web.de>
2023-02-22 05:13:50 +0100

    Refactoring and comments

--- pdftool/main.py
+++ pdftool/main.py
@@ -5,9 +5,7 @@ from operations import merge, reorderDualPage, deletePages
 
 def main():
 
-
     not_done = True
-
     while not_done:
 
         choices = {
@@ -17,14 +15,18 @@ def main():
             'Exit': exit
         }
 
+        # Prompt the user a kind of "main menu" with the options above
         style = CliMenuStyle('#CCCCCC', '#FFFFFF', '#FFFF00')
         menu = CliMenu(choices, "\nWhat do you want to do?:\n", dedent_selection=True, style=style, cursor=CliMenuCursor.ARROW)
         
         selection = menu.get_selection()[1]
 
         if(selection is not None):
+            # call the function associated with this selection.
+            # By convention the operations return True on abort. So we stay inside the loop if the user only aborted, otherwise we exit the loop and the script terminates
             not_done = choices[selection]()
         else:
+        # if selection is none the user pressed e.g. ctrl+c, exit at this point
             not_done = False
 
 if __name__ == '__main__':
--- pdftool/operations/merge.py
+++ pdftool/operations/merge.py
@@ -16,24 +16,33 @@ def merge() -> bool:
     file_paths: List[pathlib.Path] = []
     not_done = True
     while not_done:
+
+        # print the currently selected files above the prompt
+        if len(file_paths) == 0:
+            selectedFiles = 'No files selected!'
+        else:
+            selectedFiles = ', '.join([file.name for file in file_paths])
+
+
+        # the choices the user has in this "submenu"
         choices = [
             'Add a file/directory',
             'MERGE!',
             'Abort'
         ]
 
+        # prompt one of the choices above
         style = CliMenuStyle('#CCCCCC', '#FFFFFF', '#FFFF00')
-
-        if len(file_paths) == 0:
-            selectedFiles = 'No files selected!'
-        else:
-            selectedFiles = ', '.join([file.name for file in file_paths])
-
         menu = CliMenu(choices, f'\n● MERGE\nSelected files: { selectedFiles }\n', dedent_selection=True, style=style, cursor=CliMenuCursor.ARROW)
         selection = menu.get_selection()
+        # selection is none if user presses e.g. ctrl+c, exit at this point
         if selection[0] is None:
             exit()
+        
+        # choices 0-2 correspond to choices above 
         elif selection[0] == 0:
+            
+            # prompt user for a file/dir to be added, check existence
             file = prompt('\nEnter a file or directory (leave empty for current directory): ', completer=FileCompleter())
             path = pathlib.Path(file).absolute()
             if path.exists():
@@ -41,30 +50,39 @@ def merge() -> bool:
             else:
                 print('File does not exist! Ignoring.')
         elif selection[0] == 1:
+
+            # prompt output file name, if none given use default, then call __merge
             output_file = prompt('\nEnter output file name (leave empty for default \'merge-output.pdf\'): ')
             if len(output_file) == 0:
                 output_file = 'merge-output.pdf'
             __merge(file_paths, output_file)
             return False
         elif selection[0] == 2:
+            # return true on abort so main nows we aborted and does not exit
             return True
     
 
 
-
 def __merge(file_paths: List[pathlib.Path], output_file_name: str) -> None:
+    """
+    Merges the given files into a single one and writes the result into the given output file.
+    Checks if file are of type PDF. If a directory is found, it scans inside for PDFs and uses them.
+    Wraps beautiful progress bars around everything.
+    """
 
     all_pdf_paths: List[pathlib.Path] = []
 
     with patch_stdout():
         with ProgressBar(title='Scanning files...') as pb:
             for fp in pb(file_paths):
+                # check if file and pdf
                 if fp.is_file():
                     if fp.suffix == '.pdf':
                         all_pdf_paths.append(fp)
                         print(f'Added: {fp}')
                     else:
                         print(f'Not a PDF: {fp}')
+                # if directory, scan for pdfs in the dir
                 elif fp.is_dir():
                     print(f'Found directory! Searching PDFs in: {fp}')
                     for fpd in [pathlib.Path(file) for file in os.listdir()]:
@@ -74,6 +92,7 @@ def __merge(file_paths: List[pathlib.Path], output_file_name: str) -> None:
                                 print(f'Found PDF! Added: {fp}')
 
     with patch_stdout():
+        # use pypdf2 pdf merger to merge the pdfs
         merger = PdfMerger()
         with ProgressBar(title='Merging...') as pb:
             for fp in pb(all_pdf_paths):
