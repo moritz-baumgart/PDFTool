477983fc4fa92c6db7e8a8a9b8acfa4bc9f9837c
orangeUNNIX <baumgartmoritz@web.de>
2023-02-22 02:41:56 +0100

    Switched to clintermission

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from PyInquirer import prompt
 from merge import merge
 from reorderDualPage import reorderDualPage
 from deletePages import deletePages
 
+from clintermission import CliMenu, CliMenuStyle, CliMenuCursor
+
 def main():
 
     choices = {
-        '- Merge files': merge,
-        '- Reorder dual page scan': reorderDualPage,
-        '- Delete single pages': deletePages
+        'Merge files': merge,
+        'Reorder dual page scan': reorderDualPage,
+        'Delete single pages': deletePages
     }
 
-    question = [
-        {
-            'type': 'list',
-            'name': 'operation',
-            'message': 'What do you want to do?',
-            'choices': choices.keys()
-        }
-    ]
+    style = CliMenuStyle('#CCCCCC', '#FFFFFF', '#FFFF00')
+    menu = CliMenu(choices, "Time to choose:\n", dedent_selection=True, style=style, cursor=CliMenuCursor.ARROW)
+    
+    selection = menu.get_selection()[1]
 
-    operation = prompt(question)['operation']
-    choices[operation]()
+    if(selection is not None):
+        choices[selection]()
+
+
+
 
 
 if __name__ == '__main__':
     main()
 
+ 
 
